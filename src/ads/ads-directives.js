02b38b11306c9a19c890017913ee0d2535f6ea44
mihdimitrov <mihdimitrov89@gmail.com>
2015-01-09 12:57:22 +0200

    some little changes

---
+++ src/ads/ads-directives.js
@@ -0,0 +1,10 @@
+angular.module('ap.ads.directives', [])
+
+.directive('apAdContent', function() {
+  return {
+    templateUrl: 'src/ads/ad-content.html',
+    scope: {
+      adObject: '='
+    }
+  }
+})
